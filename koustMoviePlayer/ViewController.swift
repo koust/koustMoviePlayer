9645174f9c7e1527b7cb0275fb0af892fb7f23f0
Batuhan Saygılı <batuhansaygili94@gmail.com>
2018-11-08 09:44:57 +0300

    Initial commit

---
+++ koustMoviePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  koustMoviePlayer
+//
+//  Created by MacBook on 7.11.2018.
+//  Copyright © 2018 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ koustMoviePlayer/example/controller/KoustMPController.swift
@@ -0,0 +1,30 @@
+//
+//  KoustMPController.swift
+//  koustMoviePlayer
+//
+//  Created by MacBook on 7.11.2018.
+//  Copyright © 2018 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class KoustMPController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
